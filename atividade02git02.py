383f384910fec3b66ac9cfd7ef82a134bafe6c73
DevDAVIcod <162722114+DevDAVIcod@users.noreply.github.com>
2024-04-17 23:03:08 +0000

    atividade02git02.py

---
+++ atividade02git02.py
@@ -0,0 +1,4 @@
+# Crie uma laço de repetição que faça a tabuada de multiplicação do 10
+for i in range(1,10):
+    print(i*10)
+    
---
+++ atividade03git02.py
@@ -0,0 +1,4 @@
+# Crie uma lista que tenha 10 valores e some todos eles.
+lista = [1,2,3,4,5,6,7,8,9,10]
+soma = sum(lista)
+print(soma)
